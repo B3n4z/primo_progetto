339f1467a3edead5c63862e32eb4805e785eb4bd
B3n4z <giuliobenati@outlook.com>
2026-03-18 15:22:15 +0100

    update delle foto e css mancante

--- js-dom-intro/snack-9-BONUS/script.js
+++ js-dom-intro/snack-9-BONUS/script.js
@@ -7,15 +7,14 @@ const attori = [
     {nome: "marco", immagine: "marco.png", eta: "28", desc: "specializzato in film d'azione"},
     {nome: "giulia", immagine: "giulia.png", eta: "25", desc: "bravissima nei ruoli drammatici"},
     {nome: "sofia", immagine: "sofia.png", eta: "30", desc: "attrice elegante e carismatica"},
-    {nome: "andrea", immagine: "andrea.png", eta: "40", desc: "esperto e molto apprezzato dal pubblico"}
+    {nome: "andrea", immagine: "andrea.png", eta: "40", desc: "esperto e molto apprezzato dal pubblico"},
+    {nome: "mauro", immagine: "mauro.png", eta: "55", desc: "mega contadino spacca verze"}
 ];
 
 let page = document.getElementById("attori");
 
 for(let i = 0; i < attori.length; i++) {
-    page.innerHTML += `<div class="attore"><img id="${attori[i].nome}" src="${attori[i].immagine}" alt="Foto di ${attori[i].nome}">
-    <p>${attori[i].nome}</p>
-    <p>${attori[i].eta}</p>
-    <p>${attori[i].desc}</p>
+    page.innerHTML += `<div class="attore"><img id="${attori[i].nome}" src="${attori[i].immagine}" alt="Foto di ${attori[i].nome}" class="img_attori">
+    <p>${attori[i].nome} ${attori[i].eta} anni: ${attori[i].desc}</p>
     </div>`
 }
